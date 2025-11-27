e6eecc5b35f7ff0bfe7b326650f8b096b7a90048
kingsan2012 <kingsan2012@gmail.com>
2025-11-27 09:50:28 +0330

    App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Calculator from "./components/Calculator.jsx";
+
+export default function App() {
+  return (
+    <div className="container">
+      <h1>محاسبه سهم الارث</h1>
+      <Calculator />
+    </div>
+  );
+}
